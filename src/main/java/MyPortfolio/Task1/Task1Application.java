86d3d8356cbcfec1d1840bfc22c8f1578f69b10c
Priyanka-MP <priyanka.mp2731@gmail.com>
2024-07-09 16:25:26 +0530

    Git-update

---
+++ src/main/java/MyPortfolio/Task1/Task1Application.java
@@ -0,0 +1,13 @@
+package MyPortfolio.Task1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task1Application.class, args);
+	}
+//my application -portfolio
+}
